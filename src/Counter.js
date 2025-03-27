8f792f04c7d3595be20b67b41ca5804c1dec1578
xchbx <ceina.ellison@gmail.com>
2025-03-27 17:48:21 -0400

    call the increment & decrement functions in counter

--- src/Counter.js
+++ src/Counter.js
@@ -1,14 +1,11 @@
-import { useState } from "react";
 
 function Counter(props) {
-  const [count, setCount] = useState(0);
-
   return (
     <div className="Counter">
       <small>{props.label}</small>
-      <h1>{count}</h1>
-      <button onClick={() => setCount(count + 1)}>+</button>
-      <button onClick={() => setCount(count - 1)}>-</button>
+      <h1>{props.value}</h1>
+      <button onClick={props.increment}>+</button>
+      <button onClick={props.decrement}>-</button>
     </div>
   )
 };
