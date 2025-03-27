789de0b38218a15ca1bf6ba446f7704a927b084d
xchbx <ceina.ellison@gmail.com>
2025-03-27 17:37:49 -0400

    move state to App.js(List State)

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
+import { useState } from 'react';
 import './App.css';
 import Counter from './Counter';
 
 function App() {
+  const [counts, setCounts] = useState([1, 4, 3]);
+  
   return (
     <div className="App">
-      <Counter label="Cherry" value={1} />
-      <Counter label="Oranges" value={3} />
+      {counts.map((value, index) => (
+        <Counter key={index} label={`Counter ${index + 1}`} value={value} />
+      ))}
     </div>
   );
 }
