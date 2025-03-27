54b81987ca56e153a73fb8ddf7a0cb6d2b7dd30b
xchbx <ceina.ellison@gmail.com>
2025-03-27 17:45:37 -0400

    pass increment and decrement Functions as Props

--- src/App.js
+++ src/App.js
@@ -5,10 +5,22 @@ import Counter from './Counter';
 function App() {
   const [counts, setCounts] = useState([1, 4, 3]);
   
+  const updateCount = (index, delta) => {
+    const newCounts = [...counts]; //Copy array (immutability)
+    newCounts[index] += delta;
+    setCounts(newCounts);
+  };
+
   return (
     <div className="App">
       {counts.map((value, index) => (
-        <Counter key={index} label={`Counter ${index + 1}`} value={value} />
+        <Counter 
+          key={index} 
+          label={`Counter ${index + 1}`} 
+          value={value} 
+          increment={() => updateCount(index, 1)}
+          decrement={() => updateCount(index, -1)}
+          />
       ))}
     </div>
   );
